352bfb3aa98f70f6068a3731507b8153f91e0687
a1 <omerf.ay7@gmail.com>
2022-02-01 23:55:18 +0300

    Insertion sort algorithm

---
+++ src/InsertionSort.java
@@ -0,0 +1,27 @@
+public class InsertionSort {
+    /**
+     * this method sorts array in place
+     * @param arr unsorted array
+     * @return arr sorted array
+     */
+    static int[] sort(int[] arr){
+        // iterate array to sort all of them
+        // we start with index 1 because
+        // first element of array already sorted
+        for (int i=1;i<arr.length;i++){
+            int value=arr[i];
+            // we define 'j' to iterate left-hand side array
+            // which is already sorted
+            int j=i-1;
+            // move one right side each element bigger than our value
+            while (j>=0 && arr[j]>value){
+                arr[j+1]=arr[j];
+                j=j-1;
+            }
+            // while nothing to left to move
+            // replace value it's right index
+            arr[j+1]=value;
+        }
+        return arr;
+    }
+}
